8ba5f42cfdaef6c8c0610a16c883f566155adbaa
Ali Eslam <alizaeda92@gmail.com>
2021-09-30 17:25:59 +0200

    Final Project

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React from "react";
 import "./App.scss";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/HomePage/Home";
+import Services from "./components/pages/Services/Services";
+import Products from "./components/pages/Products/Products";
+import SignUp from "./components/pages/SignUp/SignUp";
+import Footer from "./components/pages/Footer/Footer";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-      <Switch>{/* <Route path="/" exact component={{}} /> */}</Switch>
+      <ScrollToTop>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/services" component={Services} />
+          <Route path="/products" component={Products} />
+          <Route path="/sign-up" component={SignUp} />
+        </Switch>
+      </ScrollToTop>
+      <Footer />
     </Router>
   );
 };
---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router";
+
+export default function ScrollToTop({ children }) {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
+  return <>{children}</>;
+}
